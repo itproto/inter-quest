63c471992430ef633b07af6458069e173904ef67
Oleksandr Kulinchenko <itprotoltd@gmail.com>
2017-10-14 00:45:26 +0100

    Initial commit.

---
+++ array/index.js
@@ -0,0 +1,51 @@
+const test = (desc, cb) =>{
+    console.log(desc);
+    cb();
+    console.log('----');
+};
+//https://github.com/yangshun/tech-interview-handbook/blob/master/algorithms/array.md
+//1. In an arrays of arrays, e.g. given [[], [1, 2, 3], [4, 5], [], [], [6, 7], [8], [9, 10], [], []],
+// print: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+const printNestedArray = (arr) => {
+   const res = [];
+   arr.forEach((innerArr) => {
+        innerArr.forEach( (el) => {
+            res.push(el);
+        });
+   });
+   console.log(res);
+}
+const arr1 = [[], [1, 2, 3], [4, 5], [], [], [6, 7], [8], [9, 10], [], []];
+test('Array. Task 1', () => printNestedArray(arr1));
+
+//2.Implement an iterator that supports hasNext(), next() and remove() methods.
+const iter = (arr) => {
+    let idx = 0;
+
+    return {
+        next: () => {
+            idx++;
+            return arr[idx];
+        },
+        hasNext: () => {
+            return idx+1 < arr.length;
+        },
+        remove: () => {
+            arr.splice(idx, 1);
+        }
+    };
+};
+
+test('Task 2', () => {
+   const it = iter([1, 2, 3, 4, 5, 6]);
+   const res = [];
+   while(it.hasNext()){
+       res.push(it.next());
+   }
+   console.log(res);
+});
+
+//3. Given a list of item prices, find all possible combinations of items that sum a particular value K.
+const itemPrices = {
+
+};
